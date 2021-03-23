7265ab1dc93151e81846bb06e324076bd325e038
AKaufhold <akaufhold1986@gmail.com>
2021-03-23 16:11:16 +0100

    Add New Controller

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,7 +9,7 @@ call_user_func(
             'SfSeolighthouse',
             'Showlighthouse',
             [
-                \Stackfactory\SfSeolighthouse\Controller\LighthouseStatisticsController::class => 'list, show, delete, analyse'
+                \Stackfactory\SfSeolighthouse\Controller\LighthouseStatisticsController::class => 'new, list, show, delete, analyse'
             ],
             // non-cacheable actions
             [
