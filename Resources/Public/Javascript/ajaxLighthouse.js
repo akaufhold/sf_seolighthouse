800c5ef4e8fa81bd4bfbbad0991e21460d90bad9
AKaufhold <akaufhold1986@gmail.com>
2021-03-24 14:13:35 +0100

    Ajax Update

--- Resources/Public/Javascript/ajaxLighthouse.js
+++ Resources/Public/Javascript/ajaxLighthouse.js
@@ -1,12 +1,30 @@
-function getLighthouseData(selectFieldObj) {
-    $.ajax({
-        url: actionsPathFromViewHelperSetInTheView,
-        data:{
-            "tx_yourextkey_yourplugin[uid]":selectFieldObj.value
-        },
-        success:function (data) {
-            // do something with your json
-            alert('Load was performed.');
+$( document ).ready(function() {
+    $('.getLighthouseData').on('click', function(){
+        var url = jQuery(this).data('url');
+        var format = 'html';
+    
+        // if set use the format from the data attribute
+        if(jQuery(this).data('format')){
+            format = jQuery(this).data('format');
         }
+    
+        // send request
+        jQuery.ajax({
+        type: "POST",
+        url: url,
+        headers: {
+            'Cache-Control': 'no-cache, no-store, must-revalidate',
+            'Pragma': 'no-cache',
+            'Expires': '0'
+        },
+        dataType: format,
+        data: data,
+        success: function (content) {
+            // do something with your loaded content
+            // remove old more-button
+            // init new more-buttons
+        }
+        });
     });
-}
+});
+ 
