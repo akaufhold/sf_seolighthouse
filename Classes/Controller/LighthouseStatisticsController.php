4ca9743e177169fdf8aeb0cb50d3e8e03c50c967
AKaufhold <akaufhold1986@gmail.com>
2021-03-24 20:11:21 +0100

    Google Pagespeed Api Implementation

--- Classes/Controller/LighthouseStatisticsController.php
+++ Classes/Controller/LighthouseStatisticsController.php
@@ -143,6 +143,8 @@ class LighthouseStatisticsController extends \TYPO3\CMS\Extbase\Mvc\Controller\A
         $ajaxGetUrlMobile  = $this->getTargetUrl($this->locale,$lightHouseGetUrl,"mobile");
 
         //\TYPO3\CMS\Core\Utility\DebugUtility::debug($ajaxGetUrlMobile); 
+        
+        $this->view->assign('pageId', $this->pageSpeedApiUrl);
         $this->view->assign('ajaxGetUrlDesktop', $ajaxGetUrlDesktop);
         $this->view->assign('ajaxGetUrlMobile', $ajaxGetUrlMobile);
     }
