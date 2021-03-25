9656574a584b9a1108fd17338852d269e7c2aa26
AKaufhold <akaufhold1986@gmail.com>
2021-03-25 18:50:06 +0100

    Fetching Lighthouse Data

--- Resources/Public/JavaScript/sfseolighthouse.js
+++ Resources/Public/JavaScript/sfseolighthouse.js
@@ -1,43 +1,51 @@
-function runLighthouse(targetUrl) {
+function fetchLighthouseData(targetUrl) {
     fetch(targetUrl)
       .then(response => response.json())
       .then(json => {
+        if (!json.hasOwnProperty("error")){
+          $(".progressBar").find(".counterAmount").addClass("done"); 
+          const lighthouse = json.lighthouseResult;
+          const lighthouseMetrics = {
+            'fcp': lighthouse.audits['first-contentful-paint'].displayValue,            //First Contentful Paint
+            'si': lighthouse.audits['speed-index'].displayValue,                        //Speed Index
+            'tti': lighthouse.audits['interactive'].displayValue,                       //Time To Interactive
+            'fmp': lighthouse.audits['first-meaningful-paint'].displayValue,            //First Meaningful Paint
+            'fci': lighthouse.audits['first-cpu-idle'].displayValue,                    //First CPU Idle
+            'eil': lighthouse.audits['estimated-input-latency'].displayValue,           //Estimated Input Latency
+            'bi': lighthouse.environment['benchmarkIndex'].displayValue,                //Benchmark Index
+          };
+          $('counterContainer').append("<span class='totalTime'>"+lighthouse.timing.total+"</span>");
+          console.log(lighthouseMetrics);
+          return lighthouse;
+        }else{
+          var errorMessage = json.error.message;
+          $(".progressBar").find(".counterAmount").addClass("error"); 
+        }
+          
         // See https://developers.google.com/speed/docs/insights/v5/reference/pagespeedapi/runpagespeed#response
         // to learn more about each of the properties in the response object.
         /*const cruxMetrics = {
           "First Contentful Paint": json.loadingExperience.metrics.FIRST_CONTENTFUL_PAINT_MS.category,
           "First Input Delay": json.loadingExperience.metrics.FIRST_INPUT_DELAY_MS.category
         };*/
-        const lighthouse = json.lighthouseResult;
-        const lighthouseMetrics = {
-          'First Contentful Paint': lighthouse.audits['first-contentful-paint'].displayValue,
-          'Speed Index': lighthouse.audits['speed-index'].displayValue,
-          'Time To Interactive': lighthouse.audits['interactive'].displayValue,
-          'First Meaningful Paint': lighthouse.audits['first-meaningful-paint'].displayValue,
-          'First CPU Idle': lighthouse.audits['first-cpu-idle'].displayValue,
-          'Estimated Input Latency': lighthouse.audits['estimated-input-latency'].displayValue
-        };
-        return lighthouseMetrics;
+
       });
-}
+} 
  
 $(function(){ 
     var url = [];
     $('.getLighthouseData').on('click', function(){
-        alert(test); 
-        console.log($(".progressBar").find(".counterAmount"));
+        //console.log($(".progressBar").find(".counterAmount"));
         $(".progressBar").find(".counterAmount").addClass("active");
         url['Mobile'] = $(this).data('mobile');
         url['Desktop'] = $(this).data('desktop');
         var device = $("input[name=device]").filter(":checked");
         var deviceVal = $(device).val();
         var format = 'html'; 
-        // if set use the format from the data attribute
         if($(this).data('format')){
             format = jQuery(this).data('format');
         } 
-        //console.log(url[deviceVal]);
-        //var lhm = runLighthouse(url[deviceVal]);
+        var lhm = fetchLighthouseData(url[deviceVal]);
     });
 });  
   
