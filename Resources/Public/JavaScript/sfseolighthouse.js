d37c6cf8e5a9b0f325836d9729b884592cabc306
AKaufhold <akaufhold1986@gmail.com>
2021-03-26 16:25:07 +0100

    Backend List Template / Javascript Repsonse

--- Resources/Public/JavaScript/sfseolighthouse.js
+++ Resources/Public/JavaScript/sfseolighthouse.js
@@ -3,23 +3,27 @@ function fetchLighthouseData(targetUrl) {
       .then(response => response.json())
       .then(json => {
         if (!json.hasOwnProperty("error")){
-          $(".progressBar").find(".counterContainer").addClass("done"); 
+          $(".progressBar").find(".counterContainer").removeClass("progress"); 
+          $(".progressBar").find(".counterContainer").addClass("success"); 
           const lighthouse = json.lighthouseResult;
           const lighthouseMetrics = {
+            'score':lighthouse.categories.performance.score,
             'fcp': lighthouse.audits['first-contentful-paint'].displayValue,            //First Contentful Paint
             'si': lighthouse.audits['speed-index'].displayValue,                        //Speed Index
             'tti': lighthouse.audits['interactive'].displayValue,                       //Time To Interactive
             'fmp': lighthouse.audits['first-meaningful-paint'].displayValue,            //First Meaningful Paint
             'fci': lighthouse.audits['first-cpu-idle'].displayValue,                    //First CPU Idle
             'eil': lighthouse.audits['estimated-input-latency'].displayValue,           //Estimated Input Latency
-            'bi': lighthouse.environment['benchmarkIndex'].displayValue,                //Benchmark Index
+            'bi': lighthouse.environment['benchmarkIndex'],                             //Benchmark Index
           };
-          $('counterContainer').append("<span class='totalTime'>"+lighthouse.timing.total+"</span>");
+          $('.counterContainer').find(".counterTitle").append("<span class='totalTime'>"+(lighthouse.timing.total/1000).toFixed(2)+" s</span>");
           console.log(lighthouseMetrics);
           return lighthouse;
         }else{
           var errorMessage = json.error.message;
+          $(".progressBar").find(".counterContainer").removeClass("progress"); 
           $(".progressBar").find(".counterContainer").addClass("error"); 
+          $(".progressBar").find(".errorMessage").append(": "+errorMessage.substring(0, 70));
         }
           
         // See https://developers.google.com/speed/docs/insights/v5/reference/pagespeedapi/runpagespeed#response
@@ -36,7 +40,7 @@ $(function(){
     var url = [];
     $('.getLighthouseData').on('click', function(){
         //console.log($(".progressBar").find(".counterAmount"));
-        $(".progressBar").find(".counterAmount").addClass("active");
+        $(".progressBar").find(".counterContainer").addClass("progress");
         url['Mobile'] = $(this).data('mobile');
         url['Desktop'] = $(this).data('desktop');
         var device = $("input[name=device]").filter(":checked");
