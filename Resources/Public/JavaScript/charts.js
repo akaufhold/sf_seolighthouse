64a24b4b3b85604950c3d60e34055d586e634eb7
AKaufhold <akaufhold1986@gmail.com>
2021-03-29 22:08:55 +0200

    Additional Audits / Charts.js Implementation / Tests

---
+++ Resources/Public/JavaScript/charts.js
@@ -0,0 +1,28 @@
+/*var datasetArray = [{
+    label: 'My First dataset',
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45]
+}];
+
+var labelArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+$(function(){ 
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: labelArray,
+            datasets: datasetArray
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+    console.log(chart["data"]);
+    //chart.data.label = "Test";
+});*/
