1d3c02f6af90e74ffe607682d787537262c665dc
AKaufhold <akaufhold1986@gmail.com>
2021-03-30 21:31:30 +0200

    SCSS / Locallang

--- Resources/Public/JavaScript/sfseolighthouse.js
+++ Resources/Public/JavaScript/sfseolighthouse.js
@@ -1,5 +1,4 @@
 /* DECLARATION AUDIT OBJECTS */
-
 const mainAudits        = [
           ["first-contentful-paint", "fcp"],
           ["speed-index", "si"],
@@ -20,11 +19,11 @@ function fetchLighthouseData(targetUrl) {
       .then(response => response.json())
       .then(json => {
         if (!json.hasOwnProperty("error")){
+          console.log(json);
           const lighthouse = json.lighthouseResult;
           const auditResults = lighthouse.audits;
           var OutputAuditsHtml = "";
           var OutputAuditName;
-
           $(".progressBar").find(".counterContainer").removeClass("progress").addClass("success"); 
           $(".progressBar").find(".counterContainer").find(".counterAmount").css({width:"100%"});
           /* MAIN AUDIT PROPERTIES */
@@ -52,26 +51,34 @@ function fetchLighthouseData(targetUrl) {
           Object.keys(auditResults).sort().forEach(function(key){
             var displayMode = String(auditResults[key].scoreDisplayMode);
             if (displayMode!="notApplicable"){
-              //console.log(String(auditResults[key].scoreDisplayMode));
               OutputAuditName   = key.replace("-"," ");
               OutputAuditsHtml += '<li class="list-group-item" id="'+key+'">';
               OutputAuditsHtml +=     '<span class="label">';
-              OutputAuditsHtml +=         OutputAuditName;
               if (auditResults[key].description){
-                  OutputAuditsHtml += '<i class="arrowDown"></i>';
+                  OutputAuditsHtml += chevronDown;
               }
+              OutputAuditsHtml +=         OutputAuditName;
               OutputAuditsHtml +=     '</span>';
-              OutputAuditsHtml +=     '<span class="value">';
-              OutputAuditsHtml +=         auditResults[key].displayValue;
-              OutputAuditsHtml +=     '</span>';
-              OutputAuditsHtml +=     '<span class="score ';
-              if (auditResults[key].score < 0.5){OutputAuditsHtml += 'slow';}
-              else if (auditResults[key].score < 0.9){OutputAuditsHtml += 'average';}
-              else if (auditResults[key].score <= 1){OutputAuditsHtml += 'fast';}
-              OutputAuditsHtml +=     '">'+auditResults[key].score;
-              OutputAuditsHtml +=     '</span>';
+              if (auditResults[key].displayValue!=undefined){
+                  OutputAuditsHtml +=     '<span class="value">';
+                  OutputAuditsHtml +=         auditResults[key].displayValue;
+                  OutputAuditsHtml +=     '</span>';
+              }
+              if (auditResults[key].score){
+                OutputAuditsHtml +=     '<span class="score ';
+                if (auditResults[key].score < 0.5){OutputAuditsHtml += 'slow';}
+                else if (auditResults[key].score < 0.9){OutputAuditsHtml += 'average';}
+                else if (auditResults[key].score <= 1){OutputAuditsHtml += 'fast';}
+                OutputAuditsHtml +=     '">'+auditResults[key].score;
+                OutputAuditsHtml +=     '</span>';
+              }
               if (auditResults[key].description){  
-                  OutputAuditsHtml += "<span class='description'>"+((auditResults[key].title)?"<b>"+auditResults[key].title+"</b>":"")+auditResults[key].description+"</span>";
+                  OutputAuditsHtml += "<span class='description'>"+((auditResults[key].title)?"<b>"+auditResults[key].title+"</b>":"")+auditResults[key].description;
+                  if (auditResults[key].hasOwnProperty("details.screenshot")){
+                    console.log(auditResults[key]+""+auditResults[key].details.screenshot);
+                      OutputAuditsHtml += auditResults[key].details.screenshot
+                  }
+                  OutputAuditsHtml += "</span>";
               }
               OutputAuditsHtml +=  '</li>';
             }
@@ -82,7 +89,6 @@ function fetchLighthouseData(targetUrl) {
             $('.counterContainer').find(".counterTitle").append("<span class='totalTime'>"+(lighthouse.timing.total/1000).toFixed(2)+" s</span>");
           else
             $('.counterContainer').find(".totalTime").html((lighthouse.timing.total/1000).toFixed(2)+" s");
-          console.log(json);
         }else{
           /* ERROR HANDLING */
           var errorMessage = json.error.message;
@@ -90,18 +96,16 @@ function fetchLighthouseData(targetUrl) {
           $(".progressBar").find(".counterContainer").find(".counterAmount").css({width:"100%"});
           $(".progressBar").find(".errorMessage").append(": "+errorMessage.substring(0, 130));
         }
-          
-        // See https://developers.google.com/speed/docs/insights/v5/reference/pagespeedapi/runpagespeed#response
-        // to learn more about each of the properties in the response object.
-        /*const cruxMetrics = {
-          "First Contentful Paint": json.loadingExperience.metrics.FIRST_CONTENTFUL_PAINT_MS.category,
-          "First Input Delay": json.loadingExperience.metrics.FIRST_INPUT_DELAY_MS.category
-        };
-        console.log(cruxMetrics); */
       });
 } 
- 
+var chevronDown;
 $(function(){ 
+    requirejs(['TYPO3/CMS/Backend/Icons'], function(Icons) {
+      // Get a single icon
+      Icons.getIcon('actions-chevron-down', Icons.sizes.small).done(function(icon) {
+        chevronDown = icon;
+      });
+    });
     var url = [];
     $('.getLighthouseData').on('click', function(){
         /* PROGRESS BAR */
@@ -123,6 +127,13 @@ $(function(){
     $("input[type=radio][name=device]").change(function(){
       var deviceName = $(this).val();
       var targetUrl = $(".getLighthouseData").data(deviceName);
-    })
+    });
+    $(".list-lighthouse").on("click","li",function(){
+      var listItem = $(this);
+      $(".list-lighthouse").find("li").removeClass("active");
+      if (!$(listItem).hasClass("active")){
+        $(listItem).addClass("active");
+      }
+    });
 });  
   
