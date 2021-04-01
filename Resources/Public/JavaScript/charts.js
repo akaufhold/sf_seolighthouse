5cc04ace0abed88abba9d1f548226118237c5fbb
AKaufhold <akaufhold1986@gmail.com>
2021-04-01 21:25:22 +0200

    Charts.js Functions

--- Resources/Public/JavaScript/charts.js
+++ Resources/Public/JavaScript/charts.js
@@ -1,36 +1,49 @@
 requirejs(['jquery'], function ($) {
-    var entries = $(".chartsEntries").find(".entry"); 
-    var data;
-    $(entries).each(function(key,value) {
-        data = $(this).data();
-    });
-    
-    var datasetArray = [{
-        label: 'My First dataset',
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45]
-    }];
 
-    var labelArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    var LighthouseData = function () {
+        var datasetArray;
+        var labelArray;
 
-    var ctx = document.getElementById('lighthouseChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
+        me.init = function () {
+            var entries = $(".chartsEntries").find(".entry"); 
+            var data;
+            var ctx = document.getElementById('lighthouseChart').getContext('2d');
 
-        // The data for our dataset
-        data: {
-            labels: labelArray,
-            datasets: datasetArray
-        },
+            $(entries).each(function(key,value) {
+                data = $(this).data();
+            });
 
-        // Configuration options go here
-        options: {}
-    });
-    //console.log(chart["data"]);
-    //chart.data.label = "Test";
 
+        }
+        /* TEST DATA */
+        me.dataset = function () {
+            var datasetArray = [{
+                label: 'My First dataset',
+                backgroundColor: 'rgba(0, 0, 0, 0)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }];
+        }
+        me.dataset = function () {
+            label = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+        }
+        
+        me.chart = function () {
+            var chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'line',
+
+                // The data for our dataset
+                data: {
+                    labels: labelArray,
+                    datasets: datasetArray
+                },
+
+                // Configuration options go here
+                options: {}
+            });
+        }
+    }
     $(document).ready(function () {
         var lighthouseCharts = new LighthouseCharts();
         lighthouseCharts.init();
