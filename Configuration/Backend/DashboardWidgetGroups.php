0a9c31f652139f6b20589a414d0a016c176f5a3e
akaufhold <info@stackfactory>
2022-04-29 18:35:39 +0200

    [ADD] widget group configuration

---
+++ Configuration/Backend/DashboardWidgetGroups.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'general' => [
+        'title' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widget_group.general',
+    ],
+    'systemInfo' => [
+        'title' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widget_group.system',
+    ],
+    'typo3' => [
+        'title' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widget_group.typo3',
+    ],
+    'news' => [
+        'title' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widget_group.news',
+    ],
+    'documentation' => [
+        'title' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widget_group.documentation',
+    ],
+    'seo' => [
+        'title' => 'LLL:EXT:sf_seolighthouse/Resources/Private/Language/locallang.xlf:tx_sf_seolighthouse_showlighthouse.widget.tab.seo',
+    ],
+];
