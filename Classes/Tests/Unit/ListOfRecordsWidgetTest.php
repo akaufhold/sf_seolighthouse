bcff79cd6739ddb0402828d5392b04e6d821367b
akaufhold <info@stackfactory>
2022-05-11 19:19:10 +0200

    [ADD] unit test for widget

---
+++ Classes/Tests/Unit/ListOfRecordsWidgetTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace Stackfactory\SfSeolighthouse\Tests\Unit;
+
+use \TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/*
+  Test
+	*/
+
+class ListOfRecordsWidgetTest extends UnitTestCase {
+	/*
+	@test
+	*/
+	public function works() :void {
+		self:assertSame(4,2+2);
+	}
+}
+	
