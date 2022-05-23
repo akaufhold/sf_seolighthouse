98860eb381045b2fb4dc11a7d7efc6e7de7c4bc9
akaufhold <info@stackfactory>
2022-05-23 19:28:02 +0200

    [ADD] testing framework

--- Classes/Tests/Unit/ListOfRecordsWidgetTest.php
+++ Classes/Tests/Unit/ListOfRecordsWidgetTest.php
@@ -4,18 +4,36 @@ declare(strict_types=1);
 
 namespace Stackfactory\SfSeolighthouse\Tests\Unit;
 
-use \TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+use \PHPUnit\Framework\TestCase;
 
-/*
-  Test
-	*/
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+use \Stackfactory\SfSeolighthouse\Widgets\ListOfRecordsWidget;
 
+use TYPO3\CMS\Dashboard\Widgets\AdditionalCssInterface;
+use TYPO3\CMS\Dashboard\Widgets\ButtonProviderInterface;
+use TYPO3\CMS\Dashboard\Widgets\RequireJsModuleInterface;
+use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
+use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use \TYPO3\CMS\Backend\Utility\BackendUtility;
+use Stackfactory\SfSeolighthouse\Controller;
+
+/** 
+  *Test
+	**/
 class ListOfRecordsWidgetTest extends UnitTestCase {
-	/*
-	@test
-	*/
-	public function works() :void {
-		self:assertSame(4,2+2);
+
+	protected $lor = null;
+
+	/** 
+	*@test
+	*@dataProvider renderWidgetContent
+	**/
+	public function TestrenderWidgetContent() :void {
+    $slug       = BackendUtility::getPreviewUrl($this->pageId);
+		//$this->lor = new ListOfRecordsWidget();
+		self::assertInstanceOf(ListOfRecordsWidget::class, $this->lor);
 	}
-}
-	
+}#
